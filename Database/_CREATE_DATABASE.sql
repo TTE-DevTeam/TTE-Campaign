365f04b0ce91b3dc8ac402fad2b0324b5e68984c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-12-28 22:26:03 +0100

    script to create entire database

---
+++ Database/_CREATE_DATABASE.sql
@@ -0,0 +1,9 @@
+/*Database itself*/
+CREATE DATABASE `tte-campaign` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+/*Tables*/
+START ./Tables/_CREATE_ALL_TABLES.sql;
+/*Procedures*/
+START ./Tables/_CREATE_ALL_PROCEDURES.sql;
+/*Triggers*/
+START ./Tables/_CREATE_ALL_TRIGGERS.sql;
+/*Indizes*/
